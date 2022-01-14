07329e26859147e9d3b28426bf011d191e2fc990
itzbahb <itzbahb@gmail.com>
2022-01-15 00:39:48 +0200

    updated all data

--- public/bobsFolder/Main.js
+++ public/bobsFolder/Main.js
@@ -1,4 +1,4 @@
-var {
+const {
   Course,
   hasLab,
   isLinked,
---
+++ public/bobsFolder/Update.js
@@ -0,0 +1,6 @@
+const {getCoursesAndCRNs, getElectives} = require("./Tools.js")
+
+let Term = "202220"
+
+getCoursesAndCRNs(Term)
+getElectives(Term)
